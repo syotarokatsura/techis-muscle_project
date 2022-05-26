e3939ac6f1641bc5e3fa247a3bec990d5b5990b4
k-haruka <poipoi8080ww@gmail.com>
2022-05-26 23:46:51 +0900

    Health登録

--- resources/views/taisosei.blade.php
+++ resources/views/taisosei.blade.php
@@ -7,68 +7,66 @@
     <title>Document</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!-- CSRF Token security?-->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- 独自のCSSを反映する publicの中のcssをもってくる-->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
 </head>
+
 <body>
     <!-- 体組成データ -->
+    <form action="{{ url('/taisosei') }}" method="POST">
+    @csrf
+        <table border='1'>
+        <tr>
+            <th>体重</th>
+            <td><input type="number" name="weight"></td>
+            <td>kg</td>
+        </tr>
+        <tr>
+            <th>BMI</th>
+            <td><input type="number" name="bmi"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <th>体脂肪率</th>
+            <td><input type="number" name="bfp"></td>
+            <td>%</td>
+        </tr>
+        <tr>
+            <th>胸囲</th>
+            <td><input type="number" name="chest"></td>
+            <td>cm</td>
+        </tr>
+        <tr>
+            <th>腕周り</th>
+            <td><input type="number" name="arm"></td>
+            <td>cm</td>
+        </tr>
+        <tr>
+            <th>ウエスト</th>
+            <td><input type="number" name="waist"></td>
+            <td>cm</td>
+        </tr>
+        <tr>
+            <th>大腿筋</th>
+            <td><input type="number" name="thign"></td>
+            <td>cm</td>
+        </tr>
+        <tr>
+            <th>ヒップ</th>
+            <td><input type="number" name="hip"></td>
+            <td>cm</td>
+        </tr>
+        <tr>
+            <td><div class="input-group mb-3"><input class="form-control" type="date" name="date"></div></td>
+        </tr>
+        </table>
+            <td><button class="btn" type="submit">登録</button></td> 
 
-<form action="{{ url('/taisosei')" method="POST">
-
-    <table border='1'>
-    <tr>
-        <th>体重</th>
-        <td><input type="number"></td>
-        <td>kg</td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    <tr>
-        <th>BMI</th>
-        <td><input type="number"></td>
-        <td></td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    <tr>
-        <th>体脂肪率</th>
-        <td><input type="number"></td>
-        <td>%</td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    <tr>
-        <th>胸囲</th>
-        <td><input type="number"></td>
-        <td>cm</td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    <tr>
-        <th>腕周り</th>
-        <td><input type="number"></td>
-        <td>cm</td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    <tr>
-        <th>ウエスト</th>
-        <td><input type="number"></td>
-        <td>cm</td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    <tr>
-        <th>大腿筋</th>
-        <td><input type="number"></td>
-        <td>cm</td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    <tr>
-        <th>ヒップ</th>
-        <td><input type="number"></td>
-        <td>cm</td>
-        <td><button class="btn" type="submit">登録</button></td>
-    </tr>
-    </table>
-
-</form>
-
+    </form>
 </body>
+
 </html>
